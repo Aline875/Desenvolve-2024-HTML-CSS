2edd0fa5b3e43e5397b986ad8fea1039ae4bf7a9
Aline <113183673+Aline875@users.noreply.github.com>
2024-04-03 11:07:37 -0300

    .

---
+++ node.js-1/tipo-boolean.js
@@ -0,0 +1,24 @@
+const estudante = 'Cadu';
+const estaAprovado = true;
+
+if (estaAprovado === true)
+{
+    console.log("Parabéns, boas festas!!");
+}
+
+else
+{
+    console.log("REPOVRADO!!!!!");
+}
+
+//Lembre-se que o que importa para a caondição é que o valor comparado seja verdadeiro, ou seja não importa se a variavel "estaAprovado" seja falsa, para a condição equanto os valores comparados forem iguais o resultado sempre sera verdadeiro.
+
+if(estudante === "Cadu")
+{
+    console.log(`olá, ${estudante}`)
+}
+else
+{
+    console.log("Quem é você? ")
+}
+
---
+++ node.js-1/tipo-null-undefined.js
@@ -0,0 +1,14 @@
+let nomeEstudante;
+console.log(nomeEstudante);
+console.log(typeof nomeEstudante);
+
+
+let telefoneEstudante = null;
+console.log(telefoneEstudante);
+console.log(typeof telefoneEstudante);
+
+console.log(telefoneEstudante + 3);
+
+console.log(telefoneEstudante + nomeEstudante);
+
+//Há diferença entre "null" e "undefined", pois o tipo null quer dizer que o campo existe mas não pode ser usado, já no undefined como o próprio nome diz ele ainda não foi definido ou seja ele pode ser definido mais tarde, para pegar informações do usuário por exemplo.
