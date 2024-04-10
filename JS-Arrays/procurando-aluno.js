308fbe1d5314de2e9f059737def6ba52184eac28
Aline <113183673+Aline875@users.noreply.github.com>
2024-04-10 11:51:12 -0300

    atualização do código e algumas anotações

--- JS-Arrays/procurando-aluno.js
+++ JS-Arrays/procurando-aluno.js
@@ -7,10 +7,14 @@ function exibeNomeENota (aluno)
 {
     if(lista[0].includes(aluno))
     {
+        // const alunos = lista[0];
+        // const media = lista[1];
 
-    const indice = lista[0].indexOf(aluno);
+        const [alunos, media] = lista;
 
-    const mediaAluno = lista[1][indice];
+        const indice = alunos.indexOf(aluno);
+
+        const mediaAluno = media[indice];
 
 
     console.log(` ${aluno} tem a média ${mediaAluno}.`);
@@ -26,3 +30,7 @@ function exibeNomeENota (aluno)
 exibeNomeENota("Aline")
 
 //Nessa aula nós vimos dois novos métodos o método "includes" e o método "indexOf". Onde o método includes percorre a lista e verifica se oque foi pedido na function realmente está la e o método "indexOf" verifica o número da posição dessa lista. É so dizer qual lista você quer acessar por meio desse "indexOf" caso tenha alguma dúvida veja o exemplo acima e verifique a documentaçao oficial do node.
+
+//Houve uma atualização nesse arquivo onde adicionamos duas variáveis para que o código fique mais legivel deixamos claro na variável que queremos acessar as determinadas listas como em "const alunos = lista[0];"que vai acessar a lista de alunos e a "const media = lista[1];" que acessa a media, lembre-se que isso foi acessa pela lista de duas dimensões.
+
+//Novamente o código foi atualizado para que o código fique o mais suscinto possivel. Retiramos o "const alunos = lista[0];" e o "const media = lista[1];" ja que eles utilizam "lista" duas vezes para diminuir isso nos declaramos uma nova variável "const [alunos, media] = lista;" que utiliza dos colchetes para atribuir os nomes e pegar os valores da "lista".
