10b54765721d10d5990e08433aa067378b4abea5
Aline <113183673+Aline875@users.noreply.github.com>
2024-04-11 11:02:00 -0300

    atualização do "for of

--- JS-Arrays/media-for-of.js
+++ JS-Arrays/media-for-of.js
@@ -1,8 +1,17 @@
 const notas = [10, 7.5, 8, 9,];
 
 //Esse for basicamente percorre o array e imprime cada um dos elementos
+//Não se esqueça de declarar uma váriavel para a soma e inicia-la sempre  da posição que deseja iniciar a soma. Na verdade eu acabei de descobrir que iniciamos a variável com "0" para que ele não altere o valor da soma no final já que estamos usando essa variável para fazer uma soma e caso ela não seja iniciada em 0 o número que foi atribuido a variável sera somado junto com o array. 
 
-for(let elemento of notas)
+let somaNotas = 0;
+
+for(let nota of notas)
 {
-    console.log(elemento);
+    somaNotas += nota 
 }
+
+const media = somaNotas / notas.length;
+
+console.log(`A media das notas é ${media.toFixed(1)}`);
+
+//A diferença fo "for of" para o "for" comum é que eu posso usar o "for" de forma mais flexivel.
