46b6b1fca4d5e37ec7c20f737e8c3f234a5190f5
Aline <113183673+Aline875@users.noreply.github.com>
2024-04-18 11:14:24 -0300

    atulização

--- node.js-2/index.js
+++ node.js-2/index.js
@@ -8,19 +8,28 @@ function trataErro(erro)
     throw new Error(chalk.red(erro.code, 'Não há arquivo no diretorio'));
 }
 
-
-function pegarAqruivo(caminhoArquivo)
+function pegarAqruivo(caminhoArquivo) 
 {
-    const encoding = 'utf-8';
-    fs.readFile(caminhoArquivo, encoding, (erro, texto) => 
-    {
-        if(erro)
-        {
-            trataErro(erro);
-        }
-        console.log(chalk.green(texto));
-    })
+    const encoding = 'uft-8';
+    fs.promises.readFile(caminhoArquivo, encoding)
+    .then((texto) => 
+        console.log(chalk.green(texto)))
+        
 }
+
+
+// function pegarAqruivo(caminhoArquivo)
+// {
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoArquivo, encoding, (erro, texto) => 
+//     {
+//         if(erro)
+//         {
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(texto));
+//     })
+// }
 
 pegarAqruivo('./arquivos');
 
@@ -42,3 +51,5 @@ pegarAqruivo('./arquivos');
 // O código foi atualizado no dia 18/04 para que possa se adquar caso volte um erro. código antigo ficara acima para revisão.
 
 //Criamos uma função para que possamos "tratar" desse erro no caso nos usamos o "throw" para "lançar" o erro para fora do programa para ele possa ser impresso na tela. 
+
+//Houve uma segunda atualização no dia 18/04 ás 10:56. atualizamosa a funçaõ pega arquivo para que ela funcione de forma assincrona. Para lidarmos com isso nós reescrevemos a função pega arquivos e ainda utilizaremos a bibliteca "fs" porem com outros métodos nesse caso usaremos o método "then" que diz "faça essa tarefa e então ..."
