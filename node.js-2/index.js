6e83c5e4003d60a7c4ec6d509f605680638737f2
Aline <113183673+Aline875@users.noreply.github.com>
2024-04-19 16:45:36 -0300

    19/04

--- node.js-2/index.js
+++ node.js-2/index.js
@@ -2,22 +2,42 @@ import fs from 'fs';
 import chalk from 'chalk';
 
 
+
 function trataErro(erro)
 {
     console.log(erro)
     throw new Error(chalk.red(erro.code, 'Não há arquivo no diretorio'));
 }
 
-function pegarAqruivo(caminhoArquivo) 
+//async/await
+async function pegarAqruivo(caminhoArquivo)
 {
-    const encoding = 'uft-8';
-    fs.promises.readFile(caminhoArquivo, encoding)
-    .then((texto) => 
-        console.log(chalk.green(texto)))
-        
+    try 
+    {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoArquivo, encoding)
+        console.log(chalk.green(texto));  
+    }
+    catch (erro)
+    {
+        trataErro(erro)
+    }
 }
 
+pegarAqruivo('./arquivos/texto.md')
+pegarAqruivo('./arquivos/');
 
+//Código sobre "promises"
+// function pegarAqruivo(caminhoArquivo) 
+// {
+//     const encoding = 'uft-8';
+//     fs.promises.readFile(caminhoArquivo, encoding)
+//     .then((texto) => 
+//         console.log(chalk.green(texto)))
+//         .catch((erro) => trataErro(erro))
+// }
+
+//Código sobre o tratamento de erro
 // function pegarAqruivo(caminhoArquivo)
 // {
 //     const encoding = 'utf-8';
@@ -31,8 +51,8 @@ function pegarAqruivo(caminhoArquivo)
 //     })
 // }
 
-pegarAqruivo('./arquivos');
 
+//Código sobre as bibliotecas e sobre o caminho dos arquivos
 // function pegarAqruivo(caminhoArquivo)
 // {
 //     const encoding = 'utf-8';
@@ -53,3 +73,7 @@ pegarAqruivo('./arquivos');
 //Criamos uma função para que possamos "tratar" desse erro no caso nos usamos o "throw" para "lançar" o erro para fora do programa para ele possa ser impresso na tela. 
 
 //Houve uma segunda atualização no dia 18/04 ás 10:56. atualizamosa a funçaõ pega arquivo para que ela funcione de forma assincrona. Para lidarmos com isso nós reescrevemos a função pega arquivos e ainda utilizaremos a bibliteca "fs" porem com outros métodos nesse caso usaremos o método "then" que diz "faça essa tarefa e então ..."
+
+//As chamadas promises são "promessas" do javascript de que ele vai fazer alguma coisa e sempre utilizamos o "promises" estamos falando de código assincrcono.
+
+// Falando agora sobre "async/await". Onde utilizamos o método async? utilizamos para dizer que uma função deve ser resolvida antes que o resultado retorne. Já o "await" nós usamos onde deve-se aguardar
