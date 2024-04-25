f651e93b3bfe8f110debd6a6fc092ecb4e855846
Aline <113183673+Aline875@users.noreply.github.com>
2024-04-25 12:49:10 -0300

    novo curso

---
+++ JS-POO/objetoLiteral.js
@@ -0,0 +1,15 @@
+const user = 
+{
+    nome: 'aline',
+    email: 'alinebeatriz875@gmail.com',
+    nascimento: '2024/04/25',
+    cargo: 'admin',
+    ativo: true,
+    exibirInfos: function()
+    {
+        console.log(this.nome, this.email)
+    }
+};
+
+
+// A partir de agora iremos usar o conceito de classe.
