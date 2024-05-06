0776999e39f313917b611020e7839569051faa5f
Aline <113183673+Aline875@users.noreply.github.com>
2024-05-06 10:28:59 -0300

    extends e super

--- JS-POO/User.js
+++ JS-POO/User.js
@@ -1,6 +1,6 @@
 // A partir de agora trabalharesmos com classes.
 
-class User 
+export default class User 
 {
     constructor (nome, email, nascimento, role, ativo = true)
     {
@@ -17,9 +17,12 @@ class User
     }
 }
 
-const novoUser = new User('Aline', 'aline@gmail.com', '08-08-2003')
-console.log(novoUser)
-console.log(novoUser.exibirInfos())
+
+
+// const novoUser = new User('Aline', 'aline@gmail.com', '08-08-2003')
+// console.log(novoUser)
+// console.log(novoUser.exibirInfos())
 
 // Para montar uma classe usamos a função "constructor" para definir o parametros da classe.
 // Quando estamos trabalhando dentro de uma classe não precisamos declarar a function apenas escrever o nomedelae que aplicar o que for necessário.
+//Como estamos trabalhando com modulos de exportação nós precisamos adicionar uma propriedade ao "package.json", neste caso usamos o " "type":"module" ".
