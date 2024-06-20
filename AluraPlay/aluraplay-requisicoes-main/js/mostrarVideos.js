bc4b9c06767886373e4746f84d3dba06f82799bc
Aline <113183673+Aline875@users.noreply.github.com>
2024-06-20 10:42:49 -0300

    erro consertar mais tarde

--- AluraPlay/aluraplay-requisicoes-main/js/mostrarVideos.js
+++ AluraPlay/aluraplay-requisicoes-main/js/mostrarVideos.js
@@ -1,23 +1,23 @@
 //Nesse arquivo iremos importar a conexao que criamos em "conectaApi.js". Apartir daqui trabalharemos com modularização e precisamos deixar isso claro dentro do arquivo html onde escreveremos "type = module". Veja no arquivo html.
 
-import { conectaApi } from "./conectaApi";
+import { conectaApi } from "./conectaApi.js";
 
 const lista = document.querySelector("[data-lista]")
 
 // No exemplo acima adicionamos um "data atributes" que serve pra individualizar os elementos e conseguir manipular o dom atraves deles. Como podem ver escrevemos "data-" e um nome qualquer.
 
-function constroiCard()
+function constroiCard(titulo, descricao, url, imagem)
 {
     const video = document.createElement("li");
     video.className = "videos__item";
-    video.innerHTML = ` <iframe width="100%" height="72%" src="https://www.youtube.com/embed/pA-EgOaF23I"
-    title="YouTube video player" frameborder="0"
+    video.innerHTML = ` <iframe width="100%" height="72%" src="${url}"
+    title="${titulo}" frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen></iframe>
 <div class="descricao-video">
-    <img src="./img/logo.png" alt="logo canal alura">
-    <h3>Qual é o melhor hardware para programação com Mario Souto</h3>
-    <p>236 mil visualizações</p>
+    <img src="${imagem}" alt="logo canal alura">
+    <h3>${titulo}</h3>
+    <p>${descricao}</p>
 </div>` 
 return video;
 
@@ -25,6 +25,12 @@ return video;
 
 async function listaVideo()
 {
-    const lista = await conectaApi.listaVideos();
-    
+    const listaApi = await conectaApi.listaVideos();
+    listaApi.foreach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
 }
+
+listaVideo()
+
+//no exemplo acimautilizamos o foreach para melhor entendimento vou escrever a leitura do código. "para cada item da lista da api cria-se um card (li) que foi anexada dentro da ul que estamos referencniando como 'lista'"
+
+
