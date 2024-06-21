578fe086d59c5b56c2c6e1a1a70e2a2d493fc891
Aline <113183673+Aline875@users.noreply.github.com>
2024-06-21 10:37:02 -0300

    requisição post

--- AluraPlay/aluraplay-requisicoes-main/js/conectaApi.js
+++ AluraPlay/aluraplay-requisicoes-main/js/conectaApi.js
@@ -6,9 +6,30 @@ async function listaVideos()
     //console.log(conexaoConvertida)
 }
 
-export const conectaApi = {
-    listaVideos
+async function criaVideo(titulo, descricao, url, imagem)
+{
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers:{
+            "contet-type" : "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem  
+        })
+    });
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
 }
+
+export const conectaApi = {
+    listaVideos, 
+    criaVideo
+}
+
+
 
 //listaVideos()
 
