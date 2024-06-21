6042ac7fb9a2ea3f023190389fd80976b14eeb0c
Aline <113183673+Aline875@users.noreply.github.com>
2024-06-21 11:19:41 -0300

    .

---
+++ AluraPlay/aluraplay-requisicoes-main/js/criarVideo.js
@@ -0,0 +1,22 @@
+import { conectaApi } from "../js/conectaApi.js";
+const formulario = document.querySelector("[data-formulario]")
+
+async function criarVideos()
+{
+    evento.preventDefault();
+
+    const imagem = document.querySelector("[data-imagem]").value;
+    const titulo = document.querySelector("[data-titulo]").value;
+    const url = document.querySelector("[data-url]").value;
+    const descricao = Math.floor(Math.random()*10).toString();
+
+    await conectaApi.criaVideo(titulo, descricao, url, imagem);
+
+    window.location.href= "../pages/envio-concluido.html";
+}
+
+formulario.addEventListener("submit", evento => criarVideos(evento));
+
+//A ordem dos fatores importa. Nesse caso quando importamos a função criar video devemos escrever os parametros na ordem que eles devem ser recebidos.
+
+
